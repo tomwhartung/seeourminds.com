d9efccada30ba021efe30d77700189950ac4f63d
Tom W Hartung <tomwhartung@gmail.com>
2020-08-31 12:32:15 -0600

    Updated static css, js, and src files to MDB5 production version.

--- Site/content/static/content/src/js/free/input.js
+++ Site/content/static/content/src/js/free/input.js
@@ -3,6 +3,7 @@ import Data from '../mdb/dom/data';
 import EventHandler from '../bootstrap/src/dom/event-handler';
 import Manipulator from '../mdb/dom/manipulator';
 import SelectorEngine from '../mdb/dom/selector-engine';
+import 'detect-autofill';
 
 /**
  * ------------------------------------------------------------------------
@@ -14,6 +15,7 @@ const NAME = 'input';
 const DATA_KEY = 'mdb.input';
 const CLASSNAME_WRAPPER = 'form-outline';
 const OUTLINE_INPUT = `.${CLASSNAME_WRAPPER} input`;
+const OUTLINE_TEXTAREA = `.${CLASSNAME_WRAPPER} textarea`;
 
 /**
  * ------------------------------------------------------------------------
@@ -24,8 +26,12 @@ const OUTLINE_INPUT = `.${CLASSNAME_WRAPPER} input`;
 class Input {
   constructor(element) {
     this._element = element;
+    this._label = null;
     this._labelWidth = 0;
+    this._labelMarginLeft = 0;
+    this._notchLeading = null;
     this._notchMiddle = null;
+    this._notchTrailing = null;
 
     if (this._element) {
       Data.setData(element, DATA_KEY, this);
@@ -39,77 +45,211 @@ class Input {
 
   // Public
   init() {
-    this._calculateLabelWidth();
+    this._getLabelData();
     this._applyDivs();
-    this._applyNotchWidth();
-    this._applyActiveClass();
+    this._applyNotch();
+    this._activate();
+  }
+
+  update() {
+    this._getLabelData();
+    this._getNotchData();
+    this._applyNotch();
+    this._activate();
+  }
+
+  forceActive() {
+    const input =
+      SelectorEngine.findOne('input', this._element) ||
+      SelectorEngine.findOne('textarea', this._element);
+    Manipulator.addClass(input, 'active');
   }
 
   dispose() {
+    this._removeBorder();
+
     Data.removeData(this._element, DATA_KEY);
     this._element = null;
   }
 
   // Private
-  _calculateLabelWidth() {
-    const label = SelectorEngine.findOne('label.form-label', this._element);
-    this._labelWidth = label.clientWidth * 0.8 + 8;
+  _getLabelData() {
+    this._label = SelectorEngine.findOne('label', this._element);
+    if (this._label === null) return;
+    this._getLabelWidth();
+    this._getLabelPositionInInputGroup();
+  }
+
+  _getNotchData() {
+    this._notchMiddle = SelectorEngine.findOne('.form-notch-middle', this._element);
+    this._notchLeading = SelectorEngine.findOne('.form-notch-leading', this._element);
+  }
+
+  _getLabelWidth() {
+    this._labelWidth = this._label.clientWidth * 0.8 + 8;
+  }
+
+  _getLabelPositionInInputGroup() {
+    this._labelMarginLeft = 0;
+
+    if (!this._element.classList.contains('input-group')) return;
+    const input =
+      SelectorEngine.findOne('input', this._element) ||
+      SelectorEngine.findOne('textarea', this._element);
+    const prefix = SelectorEngine.prev(input, '.input-group-text')[0];
+    if (prefix === undefined) {
+      this._labelMarginLeft = 0;
+    } else {
+      this._labelMarginLeft = prefix.offsetWidth - 1;
+    }
   }
 
   _applyDivs() {
     const notchWrapper = element('div');
     Manipulator.addClass(notchWrapper, 'form-notch');
-    const notchLeading = element('div');
-    Manipulator.addClass(notchLeading, 'form-notch-leading');
+    this._notchLeading = element('div');
+    Manipulator.addClass(this._notchLeading, 'form-notch-leading');
     this._notchMiddle = element('div');
     Manipulator.addClass(this._notchMiddle, 'form-notch-middle');
-    const notchTrailing = element('div');
-    Manipulator.addClass(notchTrailing, 'form-notch-trailing');
+    this._notchTrailing = element('div');
+    Manipulator.addClass(this._notchTrailing, 'form-notch-trailing');
 
-    notchWrapper.append(notchLeading);
+    notchWrapper.append(this._notchLeading);
     notchWrapper.append(this._notchMiddle);
-    notchWrapper.append(notchTrailing);
+    notchWrapper.append(this._notchTrailing);
     this._element.append(notchWrapper);
   }
 
-  _applyNotchWidth() {
+  _applyNotch() {
     this._notchMiddle.style.width = `${this._labelWidth}px`;
+    this._notchLeading.style.width = `${this._labelMarginLeft + 9}px`;
+
+    if (this._label === null) return;
+    this._label.style.marginLeft = `${this._labelMarginLeft}px`;
   }
 
-  _applyActiveClass(event) {
-    const input = event ? event.target : SelectorEngine.findOne('input', this._element);
+  _removeBorder() {
+    const border = SelectorEngine.findOne('.form-notch', this._element);
+    if (border) border.remove();
+  }
+
+  _activate(event) {
+    this._getElements(event);
+
+    const input = event
+      ? event.target
+      : SelectorEngine.findOne('input', this._element) ||
+        SelectorEngine.findOne('textarea', this._element);
+
     if (input.value !== '') {
       Manipulator.addClass(input, 'active');
     }
   }
 
-  _removeActiveClass(event) {
-    const input = event ? event.target : SelectorEngine.findOne('input', this._element);
+  _getElements(event) {
+    if (event) {
+      this._element = event.target.parentNode;
+      this._label = SelectorEngine.findOne('label', this._element);
+    }
+
+    if (event && this._label) {
+      const prevLabelWidth = this._labelWidth;
+      this._getLabelData();
+
+      if (prevLabelWidth !== this._labelWidth) {
+        this._notchMiddle = SelectorEngine.findOne('.form-notch-middle', event.target.parentNode);
+        this._notchLeading = SelectorEngine.findOne('.form-notch-leading', event.target.parentNode);
+        this._applyNotch();
+      }
+    }
+  }
+
+  _deactivate(event) {
+    const input = event
+      ? event.target
+      : SelectorEngine.findOne('input', this._element) ||
+        SelectorEngine.findOne('textarea', this._element);
     if (input.value === '') {
       input.classList.remove('active');
     }
   }
 
-  static applyActiveClass(instance) {
+  static activate(instance) {
     return function (event) {
-      instance._applyActiveClass(event);
+      instance._activate(event);
     };
   }
 
-  static removeActiveClass(instance) {
+  static deactivate(instance) {
     return function (event) {
-      instance._removeActiveClass(event);
+      instance._deactivate(event);
     };
+  }
+
+  static getInstance(element) {
+    return Data.getData(element, DATA_KEY);
   }
 }
 
-EventHandler.on(document, 'focus', OUTLINE_INPUT, Input.applyActiveClass(new Input()));
-EventHandler.on(document, 'input', OUTLINE_INPUT, Input.applyActiveClass(new Input()));
-EventHandler.on(document, 'blur', OUTLINE_INPUT, Input.removeActiveClass(new Input()));
+EventHandler.on(document, 'focus', OUTLINE_INPUT, Input.activate(new Input()));
+EventHandler.on(document, 'input', OUTLINE_INPUT, Input.activate(new Input()));
+EventHandler.on(document, 'blur', OUTLINE_INPUT, Input.deactivate(new Input()));
+
+EventHandler.on(document, 'focus', OUTLINE_TEXTAREA, Input.activate(new Input()));
+EventHandler.on(document, 'input', OUTLINE_TEXTAREA, Input.activate(new Input()));
+EventHandler.on(document, 'blur', OUTLINE_TEXTAREA, Input.deactivate(new Input()));
+
+EventHandler.on(window, 'shown.bs.modal', (e) => {
+  SelectorEngine.find(OUTLINE_INPUT, e.target).forEach((element) => {
+    const instance = Input.getInstance(element.parentNode);
+    if (!instance) return;
+    instance.update();
+  });
+  SelectorEngine.find(OUTLINE_TEXTAREA, e.target).forEach((element) => {
+    const instance = Input.getInstance(element.parentNode);
+    if (!instance) return;
+    instance.update();
+  });
+});
+
+EventHandler.on(window, 'shown.bs.dropdown', (e) => {
+  SelectorEngine.find(OUTLINE_INPUT, e.target).forEach((element) => {
+    const instance = Input.getInstance(element.parentNode);
+    if (!instance) return;
+    instance.update();
+  });
+  SelectorEngine.find(OUTLINE_TEXTAREA, e.target).forEach((element) => {
+    const instance = Input.getInstance(element.parentNode);
+    if (!instance) return;
+    instance.update();
+  });
+});
+
+EventHandler.on(window, 'shown.bs.tab', (e) => {
+  const targetId = e.target.href.split('#')[1];
+  const target = SelectorEngine.findOne(`#${targetId}`);
+  SelectorEngine.find(OUTLINE_INPUT, target).forEach((element) => {
+    const instance = Input.getInstance(element.parentNode);
+    if (!instance) return;
+    instance.update();
+  });
+  SelectorEngine.find(OUTLINE_TEXTAREA, target).forEach((element) => {
+    const instance = Input.getInstance(element.parentNode);
+    if (!instance) return;
+    instance.update();
+  });
+});
 
 // auto-init
-SelectorEngine.find(`.${CLASSNAME_WRAPPER}`).forEach((input) => {
-  new Input(input).init();
+SelectorEngine.find(`.${CLASSNAME_WRAPPER}`).forEach((element) => {
+  new Input(element).init();
+});
+
+// auto-fill
+EventHandler.on(window, 'onautocomplete', (e) => {
+  const instance = Input.getInstance(e.target.parentNode);
+  if (!instance) return;
+  instance.forceActive();
 });
 
 export default Input;
--- Site/content/static/content/src/js/mdb.free.js
+++ Site/content/static/content/src/js/mdb.free.js
@@ -3,23 +3,25 @@ import Alert from './bootstrap/src/alert';
 import Button from './bootstrap/src/button';
 import Carousel from './bootstrap/src/carousel';
 import Collapse from './bootstrap/src/collapse';
-import Dropdown from './bootstrap/src/dropdown';
 import Modal from './bootstrap/src/modal';
 import Popover from './bootstrap/src/popover';
 import ScrollSpy from './bootstrap/src/scrollspy';
 import Tab from './bootstrap/src/tab';
-import Toast from './bootstrap/src/toast';
 import Tooltip from './bootstrap/src/tooltip';
+import Toast from './bootstrap/src/toast';
 
 // MDB FREE COMPONENTS
 import Input from './free/input';
-import Animate from './free/animate';
-import Treeview from './free/treeview';
+import Dropdown from './free/dropdown';
 import Ripple from './free/ripple';
+
+// AUTO INIT
+[...document.querySelectorAll('[data-toggle="tooltip"]')].map((tooltip) => new Tooltip(tooltip));
+[...document.querySelectorAll('[data-toggle="popover"]')].map((popover) => new Popover(popover));
+[...document.querySelectorAll('.toast')].map((toast) => new Toast(toast));
 
 export {
   Alert,
-  Animate,
   Button,
   Carousel,
   Collapse,
@@ -32,5 +34,4 @@ export {
   Tab,
   Toast,
   Tooltip,
-  Treeview,
 };
